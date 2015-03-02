2dec598fb369aca8a8b87c96c9fc6dc6cfe038a3
Zeno Rocha <hi@zenorocha.com>
2015-03-02 09:33:44 -0300

    Gets repo data from GitHub

---
+++ README.md
@@ -0,0 +1,27 @@
+# Fetch
+
+TODO: Project under construction
+
+## Install
+
+```sh
+$ git clone git@github.com:customelements/fetch.git
+$ npm install
+```
+
+## Setup
+
+```sh
+$ export GITHUB_CLIENT_ID
+$ export GITHUB_CLIENT_SECRET
+```
+
+## Usage
+
+```sh
+$ node server
+```
+
+## License
+
+[MIT License](http://webcomponentsorg.mit-license.org/) © WebComponents.org
--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+var boom = require('boom');
+var env = require('./config/env.js');
 var fetch = require('./fetch');
+var GitHub = require('github');
 
 module.exports = function(request, reply) {
     fetch('https://bower-component-list.herokuapp.com/keyword/web-components')
         .then(reduce)
+        .then(combine)
         .then(reply)
         .catch(reply);
 };
@@ -19,4 +23,40 @@ function reduce(data) {
     });
 
     return reducedData;
+}
+
+function combine(repos) {
+    var promises = [];
+    var github = new GitHub({
+        version: '3.0.0',
+    });
+
+    github.authenticate({
+        type: 'oauth',
+        key: env.GITHUB_CLIENT_ID,
+        secret: env.GITHUB_CLIENT_SECRET
+    });
+
+    repos.forEach(function(repo) {
+        promises.push(
+            getRepoFromGithub(github, repo)
+        );
+    });
+
+    return Promise.all(promises);
+}
+
+function getRepoFromGithub(github, repo) {
+    return new Promise(function(resolve, reject) {
+        github.repos.get({
+            user: repo.owner,
+            repo: repo.name
+        }, function(error, result) {
+            if (error) {
+                reject(boom.create(error.code, 'Error when requesting repo: ' + repo.owner + '/' + repo.name));
+            }
+
+            resolve(result);
+        });
+    });
 }
