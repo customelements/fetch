283b1301a83670454eb5c29154de7506efdd39fa
Zeno Rocha <hi@zenorocha.com>
2015-03-02 21:22:06 -0300

    Improves logging to be more descriptive

--- routes/packages/bower.js
+++ routes/packages/bower.js
@@ -2,22 +2,26 @@ var fetch = require('../../utils/fetch');
 var Package = require('../../models/package');
 
 module.exports = function(request, reply) {
+    request.log([request.route.method], 'Request made to: ' + request.route.path);
+
     fetch('https://bower-component-list.herokuapp.com/keyword/web-components')
         .then(function(result) {
-            request.log(['promise'], 'Done with #fetch');
-            return reduce(result);
+            request.log(['#fetch'], 'Done with promise');
+            return reduce(result, request);
         })
         .then(function(result) {
-            request.log(['promise'], 'Done with #reduce');
+            request.log(['#reduce'], 'Done with promise');
             return reply(result);
         })
         .catch(reply);
 };
 
-function reduce(data) {
+function reduce(data, request) {
     var reducedData = [];
 
     data.forEach(function(elem) {
+        request.log(['#reduce'], 'Create new Package() from: ' + elem.name);
+
         reducedData.push(
             new Package({
                 name: elem.name,
