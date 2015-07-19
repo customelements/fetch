4df9446d8e9b1b0bf725ef1612d7f1c58a6919a9
Zeno Rocha <hi@zenorocha.com>
2015-07-18 19:15:49 -0700

    Displays logs on tests

--- server.js
+++ server.js
@@ -15,29 +15,29 @@ server.route(require('./routes'));
 
 // -- Start --------------------------------------------------------------------
 
-if (!module.parent) {
-    server.register({
-        register: require('good'),
-        options: {
-            reporters: [{
-                reporter: require('good-console'),
-                events: {
-                    log: '*',
-                    error: '*',
-                    request: '*',
-                    response: '*'
-                }
-            }]
-        }
-    }, function (err) {
-        if (err) {
-            throw err;
-        }
+server.register({
+    register: require('good'),
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            events: {
+                log: '*',
+                error: '*',
+                request: '*',
+                response: '*'
+            }
+        }]
+    }
+}, function (err) {
+    if (err) {
+        throw err;
+    }
 
+    if (!module.parent) {
         server.start(function () {
             server.log('info', 'Server running at: ' + server.info.uri);
         });
-    });
-}
+    }
+});
 
 module.exports = server;
